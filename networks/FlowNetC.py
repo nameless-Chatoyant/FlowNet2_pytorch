0c8c2a81f40ba4c9ac56c183a0fcf495c423a2a8
Ranhao Kang <ranhao@momenta.ai>
2018-04-28 15:03:18 +0800

    .

--- networks/FlowNetC.py
+++ networks/FlowNetC.py
@@ -58,12 +58,12 @@ class FlowNetC(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 if m.bias is not None:
-                    init.uniform(m.bias)
+                    init.uniform_(m.bias)
                 init.xavier_uniform(m.weight)
 
             if isinstance(m, nn.ConvTranspose2d):
                 if m.bias is not None:
-                    init.uniform(m.bias)
+                    init.uniform_(m.bias)
                 init.xavier_uniform(m.weight)
                 # init_deconv_bilinear(m.weight)
         self.upsample1 = nn.Upsample(scale_factor=4, mode='bilinear')
