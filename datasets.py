d947b68e06683c9cd90f38db1f8c07b8b3eece1e
Ranhao Kang <ranhao@momenta.ai>
2018-05-02 11:02:02 +0800

    .

--- datasets.py
+++ datasets.py
@@ -25,7 +25,7 @@ class StaticCenterCrop(object):
         self.th, self.tw = crop_size
         self.h, self.w = image_size
     def __call__(self, img):
-        return img[(self.h-self.th)/2:(self.h+self.th)/2, (self.w-self.tw)/2:(self.w+self.tw)/2,:]
+        return img[(self.h-self.th)//2:(self.h+self.th)//2, (self.w-self.tw)//2:(self.w+self.tw)//2,:]
 
 class MpiSintel(data.Dataset):
     def __init__(self, args, is_cropped = False, root = '', dstype = 'clean', replicates = 1):
